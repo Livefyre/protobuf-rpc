de3d74a2847049c621a64d26c091b3239fd6b19e
Ashish Sahu <asahu@adobe.com>
2017-06-21 18:28:28 -0700

    logging added in protobuf-rpc

--- python/protobuf_rpc/base_server.py
+++ python/protobuf_rpc/base_server.py
@@ -1,8 +1,13 @@
+import json
+import time
+
+from logging import getLogger
 from protobuf_rpc.controller import SocketRpcController
 from protobuf_rpc.error import MethodNotFoundError
 from protobuf_rpc.protos.rpc_pb2 import Request, Response, RPC_ERROR, \
     INVALID_REQUEST_PROTO, METHOD_NOT_FOUND, BAD_REQUEST_PROTO
 from protobuf_rpc.util import deserialize_string
+from protobuf_to_dict import protobuf_to_dict
 
 
 class Callback(object):
@@ -17,33 +22,52 @@ class Callback(object):
         self.invoked = True
 
 
+LOG_FORMAT = ''
+access_log = getLogger('protobuf_rpc.access')
+
+
 class ProtoBufRPCServer(object):
+    def __init__(self, log_format=LOG_FORMAT):
+        self.log_format = log_format
+
     def handle(self, request):
+        start = time.time()
         try:
             req_obj = self.parse_outer_request(request)
         except Exception as e:
             return self.build_error_response(e.message, INVALID_REQUEST_PROTO)
+        try:
+            logging_params = {'method': req_obj.method_name, 'request': json.dumps(protobuf_to_dict(req_obj))}
+        except Exception as e:
+            access_log.exception("Error writing to logging params")
 
         try:
             method = self.get_method(req_obj.method_name)
             if method is None:
                 raise MethodNotFoundError("Method %s not found" % (req_obj.method_name))
         except Exception as e:
-            return self.build_error_response(e.message, METHOD_NOT_FOUND, req_obj)
+            logging_params['latency'] = time.time() - start;
+            return self.build_error_response(e.message, METHOD_NOT_FOUND, req_obj, logging_params)
 
         try:
             req_proto = self.parse_inner_request(req_obj, method)
         except Exception as e:
-            return self.build_error_response(e.message, BAD_REQUEST_PROTO, req_obj)
+            logging_params['latency'] = time.time() - start;
+            return self.build_error_response(e.message, BAD_REQUEST_PROTO, req_obj, logging_params)
 
         try:
-            response = self.do_request(method, req_proto, req_obj)
+            response = self.do_request(method, req_proto)
         except NotImplementedError as e:
-            return self.build_error_response(e.message, METHOD_NOT_FOUND, req_obj)
+            logging_params['latency'] = time.time() - start;
+            return self.build_error_response(e.message, METHOD_NOT_FOUND, req_obj, logging_params)
         except Exception as e:
-            return self.build_error_response(e.message, RPC_ERROR, req_obj)
+            logging_params['latency'] = time.time() - start;
+            return self.build_error_response(e.message, RPC_ERROR, req_obj, logging_params)
 
         response.request_id = req_obj.id
+        logging_params['latency'] = time.time() - start;
+        logging_params['status_code'] = 200
+        access_log.info(self.log_format % logging_params)
         return response
 
     def parse_outer_request(self, request):
@@ -58,15 +82,17 @@ class ProtoBufRPCServer(object):
         return deserialize_string(request.request_proto,
                                   self.service.GetRequestClass(method))
 
-    def do_request(self, method, proto_request, req_obj):
-        controller = SocketRpcController(req_obj.headers)
+    def do_request(self, method, proto_request):
+        controller = SocketRpcController()
         callback = Callback()
         self.service.CallMethod(method, controller, proto_request, callback)
         response = Response()
         response.response_proto = callback.response.SerializeToString()
         return response
 
-    def build_error_response(self, error_message, error_code=RPC_ERROR, req_obj=None):
+    def build_error_response(self, error_message, error_code=RPC_ERROR, req_obj=None, logging_params=None):
+        logging_params['status_code'] = 500
+        access_log.info(self.log_format % logging_params)
         response = Response()
         if req_obj is not None:
             response.request_id = req_obj.id
